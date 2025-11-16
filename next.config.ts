847b49a044bf9464a102a6b866b082a9432896ec
Ahmadreza Ansari <ahmadreza.an.dev@gmail.com>
2025-11-16 04:28:07 +0330

    change

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
@@ -9,4 +10,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
